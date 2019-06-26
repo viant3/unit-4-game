b8a375de39314da87f450f96f4acf899de72bbdb
victor politano <viant3ucf@gmail.com>
2019-06-26 16:01:03 -0400

    added conditions to check user number against random comp number - added html, need to format for looks in css

---
+++ assets/javascript/game.js
@@ -0,0 +1,74 @@
+var userWins = 0;
+var userLosses = 0;
+var userPoints = 0;
+var userNum = 0;
+
+
+var pClicks = [];
+
+
+
+$(document).ready(function () {
+
+    randomNumberHolder = $("#random-number");
+    var randNum = Math.floor(Math.random() * 120) + 19;
+    randomNumberHolder.text(randNum);
+
+
+    function clickNum() {
+        userNum = Math.floor(Math.random() * 12) + 1;
+        pClicks.push(userNum);
+        var totalClicks = 0;
+        for (var i = 0; i < pClicks.length; i++) {
+            totalClicks += pClicks[i];
+        }
+
+        totalClicksNumberHolder = $("#total-clicks");    
+        totalClicksNumberHolder.text(totalClicks);
+
+        if (totalClicks > randNum) {
+            alert("hang on there buddy")
+            pClicks = [];
+            userLosses ++;
+            totalClicks = 0;
+            totalClicksNumberHolder.text("0");
+            
+            
+        }
+        else
+            return (userNum);
+    }
+
+
+    $("#p1-holder").on("click", function () {
+        clickNum()
+        console.log(userNum);
+
+        console.log("points so far: " + pClicks);
+
+    });
+
+    $("#p2-holder").on("click", function () {
+        clickNum()
+        console.log(clickNum());
+    });
+
+    $("#p3-holder").on("click", function () {
+        clickNum()
+        console.log(clickNum());
+    });
+
+    $("#p4-holder").on("click", function () {
+        clickNum()
+        console.log(clickNum());
+    });
+
+
+
+
+
+
+});
+
+
+
